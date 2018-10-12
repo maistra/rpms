fb69ce1792e563777ab192ec08a75d249a75c0bb
Dmitri Dolguikh <ddolguik@redhat.com>
2018-10-12 14:38:57 -0400

    Fixed recipes branch name

--- istio-proxy/fetch.sh
+++ istio-proxy/fetch.sh
@@ -22,7 +22,7 @@ function set_default_envs() {
   fi
 
   if [ -z "${RECIPES_GIT_BRANCH}" ]; then
-    RECIPES_GIT_BRANCH=maistra-0.3.0
+    RECIPES_GIT_BRANCH=maistra-0.3
   fi
 
   if [ -z "${CLEAN_FETCH}" ]; then
