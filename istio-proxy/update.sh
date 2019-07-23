e73d4195023a947225cbef6c601cab8a2779aa90
Brian Avery <bavery@redhat.com>
2019-07-23 17:42:25 -0400

    Updated as per code review comments

--- istio-proxy/update.sh
+++ istio-proxy/update.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
-NEW_SOURCES=""
-
+PROXY_SHA=""
 function usage() {
 	echo "Usage: $0 [-i <SHA of istio>]"
 	echo
 	exit 0
 }
 
-while getopts ":i:v:" opt; do
+while getopts ":i:" opt; do
 	case ${opt} in
 		i) PROXY_SHA="${OPTARG}";;
 		*) usage;;
@@ -18,27 +17,28 @@ done
 [[ -z "${PROXY_SHA}" ]] && PROXY_SHA="$(grep '%global git_commit ' istio-proxy.spec | cut -d' ' -f3)"
 
 function update_commit() {
-		sha=$1
+		local sha=$1
 		echo
-		echo "Updating spec file with $1"
+		echo "Updating spec file with ${sha}"
     sed -i "s/%global git_commit .*/%global git_commit ${sha}/" istio-proxy.spec
 }
 
 function new_sources() {
-	sha=$1
+	local filename=$1
 	echo
-	echo "Updating sources file with ${sha}"
-	md5sum ${sha} > sources
+	echo "Updating sources file with ${filename}"
+	md5sum ${filename} > sources
 }
 
 function get_sources() {
+	local proxy_sha=$1
 	FETCH_DIR=/tmp CREATE_TARBALL=true ./fetch.sh
-	TAR_NAME=istio-proxy.${PROXY_SHA}.tar.xz
-	cp -p /tmp/proxy-full.tar.xz ${TAR_NAME}
+	local tar_name=istio-proxy.${1}.tar.xz
+	cp -p /tmp/proxy-full.tar.xz ${tar_name}
 
-	new_sources ${TAR_NAME}
-	md5sum ${TAR_NAME} > sources
+	new_sources ${tar_name}
+	md5sum ${tar_name} > sources
 }
 
 update_commit "${PROXY_SHA}"
-get_sources "" "${PROXY_SHA}"
+get_sources "${PROXY_SHA}"
