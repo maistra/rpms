7e4af24291eb17bba650731cf802bff8f0fc8d67
Kevin Conner <kconner@redhat.com>
2019-09-10 09:43:06 -0700

    MAISTRA-930: updating the community configuration of grafana to more closely match product

--- grafana/update.sh
+++ grafana/update.sh
@@ -3,51 +3,31 @@
 NEW_SOURCES=""
 
 function usage() {
-    echo "Usage: $0 [-i <SHA of istio>]"
-    echo
-    exit 0
+  echo "Usage: $0 [-v <grafana version>]"
+  echo
+  exit 0
 }
 
-while getopts ":i:v:" opt; do
+while getopts ":v:" opt; do
   case ${opt} in
-    i) GRAFANA_VERSION="${OPTARG}";;
+    v) GRAFANA_VERSION="${OPTARG}";;
     *) usage;;
   esac
 done
 
-[[ -z "${GRAFANA_VERSION}" ]] && GRAFANA_VERSION="$(grep '%global version ' grafana.spec | cut -d' ' -f3)"
+[[ -z "${GRAFANA_VERSION}" ]] && GRAFANA_VERSION="$(egrep '^Version:' grafana.spec | awk '{print $2}')"
 
-function update_commit() {
-    local prefix="$1"
-    local prefix_spec=${prefix/-/_}
-    local sha="$2"
+sed -i -e '/^Version: / s+[0-9][0-9.]*$+'${GRAFANA_VERSION}'+' grafana.spec
+sed -i -e '/^\/grafana-[0-9][0-9.]*$/d' .gitignore
 
-    local tarball="https://github.com/grafana/grafana/archive/v${sha}.tar.gz"
-    local filename="v${sha}.tar.gz"
+echo "/grafana-${GRAFANA_VERSION}" >> .gitignore
 
-    echo -n "Checking Grafana...   "
-		echo "Grafana version: ${sha}"
-    if [ ! -f "${filename}" ]; then
-        echo "Downloading ${tarball}"
-        curl -Lfs ${tarball} -o "${filename}"
-        if [ $? -ne 0 ]; then
-            echo "Error downloading tarball, exiting."
-            exit 1
-        fi
-    else
-        echo "Already on disk, download not necessary"
-    fi
-
-    sed -i "s/%global version .*/%global version ${sha}/" grafana.spec
-    NEW_SOURCES="${NEW_SOURCES} ${filename}"
-}
-
-function new_sources() {
-    echo
-    echo "Updating sources file with ${NEW_SOURCES}"
-    md5sum ${NEW_SOURCES} > sources
-}
-
-update_commit "" "${GRAFANA_VERSION}"
 ./make_grafana_webpack.sh "${GRAFANA_VERSION}"
-new_sources
+SHA=$(sha256sum grafana_webpack-${GRAFANA_VERSION}.tar.gz | sed -e 's+ .*$++')
+mv grafana_webpack-${GRAFANA_VERSION}.tar.gz grafana_webpack-${GRAFANA_VERSION}.${SHA}.tar.gz
+sed -i -e 's+\(^%global  *webpack_hash  *\)[^ ]*$+\1'${SHA}'+' grafana.spec
+
+NEW_SOURCES="grafana_webpack-${GRAFANA_VERSION}.${SHA}.tar.gz grafana-${GRAFANA_VERSION}.tar.gz"
+
+echo "Updating sources file with ${NEW_SOURCES}"
+md5sum ${NEW_SOURCES} > sources
