665907083c6e4f2c43b8bde4ee7892367a71faab
Daniel Grimm <dgrimm@redhat.com>
2020-01-28 15:27:50 +0100

    MAISTRA-1122 Update prometheus to match Istio 1.4 codebase

---
+++ prometheus-promu/update.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+NEW_SOURCES=""
+
+function usage() {
+  echo "Usage: $0 [-i <version of promu>]"
+  echo
+  exit 0
+}
+
+while getopts ":i:" opt; do
+  case ${opt} in
+    i) PROMU_VERSION="${OPTARG}";;
+    *) usage;;
+  esac
+done
+
+[[ -z "${PROMU_VERSION}" ]] && PROMU_VERSION="$(grep 'Version: ' promu.spec | awk '{print $2}')"
+
+function update_version() {
+    local version="$1"
+
+    local tarball="https://github.com/prometheus/promu/archive/v${version}.tar.gz"
+    local filename="v${version}.tar.gz"
+
+    echo -n "Checking promu...   "
+    if [ ! -f "${filename}" ]; then
+        echo "Downloading ${tarball}"
+        curl -Lfs ${tarball} -o "${filename}"
+        if [ $? -ne 0 ]; then
+            echo "Error downloading tarball, exiting."
+            exit 1
+        fi
+    else
+        echo "Already on disk, download not necessary"
+    fi
+
+    sed -i "s/Version:        .*/Version:        ${version}/" promu.spec
+    NEW_SOURCES="${NEW_SOURCES} ${filename}"
+}
+
+function new_sources() {
+    echo
+    echo "Updating sources file with ${NEW_SOURCES}"
+    md5sum ${NEW_SOURCES} > sources
+}
+
+update_version "${PROMU_VERSION}"
+new_sources
